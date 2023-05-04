83ec84cbf98d20e887d5e5d88f9f7720a2ce92bf
tanjumtanha <tanha.tanni27@gmail.com>
2023-05-04 12:34:50 +0600

    another section for recipe share added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import ChefCart from './ChefCart';
+import UploadRecipe from './UploadRecipe';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <ChefCart></ChefCart>
+            <UploadRecipe></UploadRecipe>
         </div>
     );
 };
---
+++ src/components/UploadRecipe.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const UploadRecipe = () => {
+    return (
+        <Container className="my-5 py-4">
+            <h2 className='text-danger text-center p-1 mb-3' style={{backgroundColor:'lightpink'}}>Would you like to share your recipes with us?</h2>
+            <Row>
+                <Col md={6} className="text-center">
+                    <img src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg" alt="" />
+                </Col>
+                <Col md={6}>
+                    <h5 >Your Email</h5>
+                    <input type="email" name="" id="" placeholder='Your Email' style={{width:'100%',height:'30px'}}/>
+                    <br />
+                    <h5 className='mt-2'>Your Name</h5>
+                    <input type="text" name="" id=""  placeholder='Your Name' style={{width:'100%',height:'30px'}}/>
+                    <br />
+                    <h5 className='mt-2'>Photo URL</h5>
+                    <input type="text" name="" id=""  placeholder='Photo url' style={{width:'100%',height:'30px'}}/>
+                    <br />
+                    
+                    <h5 className='mt-2'>Your Recipe</h5>
+                    <input type="text" name="" id="" placeholder='Write your recipe here' style={{width:'100%',height:'100px'}}/>
+                    <br />
+                    <br />
+                    <Link to='/'><Button className='btn-danger'>Submit</Button></Link>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default UploadRecipe;
